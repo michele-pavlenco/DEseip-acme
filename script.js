3d900244ccb2353cd3e35e4ccedb4690beabc183
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-10-06 19:31:37 +0200

    fix

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 // Tra i 5.000.000 e i 10.000.000 euro, la percentuale di fee è dello 0.4 % ANNUALE.
 // Oltre i 10.000.000 di euro, la percentuale di fee è dello 0.3 % ANNUALE.
 let result = document.getElementById("result")
+let popup = document.getElementById("myPopup")
+let subscribeMessage = document.getElementById("message")
 
 function multiplyBy() {
     firstNumber = document.getElementById("firstNumber").value;
@@ -15,31 +17,37 @@ function multiplyBy() {
     } else if (firstNumber >= 15000 && firstNumber < 500000) {
         let calculation = firstNumber * 0.7 / 100
         result.innerHTML = Math.floor(calculation / 12)
-        console.log(calculation)
+        message()
+        // console.log(calculation)
     } else if (firstNumber >= 500000 && firstNumber < 1000000) {
         let calculation = ((firstNumber - 500000) * 0.6 / 100) + ((500000 * 0.7) / 100)
         result.innerHTML = Math.floor(calculation / 12)
-        console.log(calculation)
+        message()
+        // console.log(calculation)
     } else if (firstNumber >= 1000000 && firstNumber < 5000000) {
         let calculation = ((firstNumber - 1000000) * 0.5 / 100) + (500000 * 0.6 / 100) + ((500000 * 0.7) / 100)
         result.innerHTML = Math.floor(calculation / 12)
-        console.log(calculation)
+        message()
+        // console.log(calculation)
     } else if (firstNumber >= 5000000 && firstNumber < 10000000) {
         let calculation = ((firstNumber - 5000000) * 0.4 / 100) + (4000000 * 0.5 / 100) + (500000 * 0.6 / 100) + ((500000 * 0.7) / 100)
         result.innerHTML = Math.floor(calculation / 12)
-        console.log(calculation)
+        message()
+        // console.log(calculation)
     } else if (firstNumber >= 10000000) {
         let calculation = ((firstNumber - 10000000) * 0.3 / 100) + (5000000 * 0.4 / 100) + (4000000 * 0.5 / 100) + (500000 * 0.6 / 100) + ((500000 * 0.7) / 100)
         result.innerHTML = Math.floor(calculation / 12)
-        console.log(calculation)
+        message()
+        // console.log(calculation)
     }
-
-
-
-
 }
 function myFunction() {
-    let popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+    popup.classList.toggle("show")
+}
+function message() {
+    console.log("message")
+    subscribeMessage.innerHTML = ' <div><p>Prova il servizio di custodia Acme Corp: non potrai più farne a meno.</p><button class="btnlight">Parla con noi </button><button>Apri un conto</button></div>'
+    // message.innerHTML = "<div> <button>Parla con noi</button><button>Apri un conto</button></div>"
+    // /document.getElementById("message").innerHTML = "Prova il servizio di custodia Acme Corp: non potrai più farne a meno."
 }
 
